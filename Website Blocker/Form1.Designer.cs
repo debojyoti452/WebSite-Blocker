0919095f81d81aea411404c9946f3cd45ff84f74
debojyoti452 <20729878+debojyoti452@users.noreply.github.com>
2019-08-09 08:42:52 +0530

    Add files via upload

---
+++ Website Blocker/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Website_Blocker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.urlInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(347, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Block IT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // urlInput
+            // 
+            this.urlInput.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.urlInput.Location = new System.Drawing.Point(154, 174);
+            this.urlInput.Name = "urlInput";
+            this.urlInput.Size = new System.Drawing.Size(614, 22);
+            this.urlInput.TabIndex = 1;
+            this.urlInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(933, 468);
+            this.Controls.Add(this.urlInput);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Website Blocker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox urlInput;
+    }
+}
+
---
+++ Website Blocker/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Website_Blocker
+{
+    public partial class Form1 : Form
+    {
+        private String inputURL;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                inputURL = "\n127.0.0.1   " + urlInput.Text;
+                using (StreamWriter w = File.AppendText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), @"drivers\etc\hosts")))
+                {
+                    w.WriteLine(inputURL);
+                    MessageBox.Show("Successfully done...");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);   
+            }
+
+        }
+
+    }
+}
